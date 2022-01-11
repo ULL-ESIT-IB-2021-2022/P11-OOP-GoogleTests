ae0f773b76904b79c979aca5800a66b256cae255
fsande <fsande@ull.es>
2022-01-11 18:17:06 +0000

    AreEqual() function added in tests

--- vector3D/test/test_vector.cc
+++ vector3D/test/test_vector.cc
@@ -1,34 +1,45 @@
 #include <gtest/gtest.h>
+#include <cmath> // fabs
 
 #include "vector.h"
 
 /**
+ * @brief Return true if both parameters are (approximately) equal 
+ * @param[in] number1 First number to compare
+ * @param[in] number2 Second number to compare
+ * @param[in] epsilon Precision for the comparison. It defaults
+ * @see https://www.tutorialspoint.com/floating-point-comparison-in-cplusplus
+ * @see https://stackoverflow.com/a/17341/12791643
+ */
+bool AreEqual(const double number1, const double number2, const double epsilon = 1e-10) {
+  const bool result = fabs(number1 - number2) <= ((fabs(number1) < fabs(number2) ? fabs(number2) : fabs(number1)) * epsilon);
+  // if (result == false) {  // Para depuración
+  //   std::cout << std::setprecision(20);
+  //   std::cout << "Epsilon:    " << epsilon << std::endl;
+  //   std::cout << "Número1:    " << number1 << std::endl;
+  //   std::cout << "Número2:    " << number2 << std::endl;
+  //   std::cout << "Difference: " << fabs(number1 - number2) << std::endl;
+  // }
+  return result;
+}
+
+/**
  * Test para probar los constructores de la clase
- * No se comprueba la igualdad sobre números de tipo double por los
- * problemas de precisión.
- *
- * Se prueba que el valor devuelto sea igual al esperado por una diferencia
- * no mayor de epsilon (kEpsilon), un valor cercano a cero.
- * 
- * @See https://stackoverflow.com/questions/12278523/comparing-double-values-in-c
- *
  */
 TEST(VectorTest, constructor) {
-  const double kEpsilon = 1e-10;   // Un valor muy pequeño; prácticamente cero.
   Vector my_vector;
 
-  EXPECT_EQ(true, my_vector.x() < kEpsilon);
-  EXPECT_EQ(true, my_vector.y() < kEpsilon);
-  EXPECT_EQ(true, my_vector.z() < kEpsilon);
+  EXPECT_EQ(true, AreEqual(my_vector.x(), 0.0));
+  EXPECT_EQ(true, AreEqual(my_vector.y(), 0.0));
+  EXPECT_EQ(true, AreEqual(my_vector.z(), 0.0));
 
   Vector another_vector{1.0, 2.0, 3.0};
-  EXPECT_EQ(true, (my_vector.x() - 1.0 < kEpsilon));
-  EXPECT_EQ(true, (my_vector.y() - 2.0 < kEpsilon));
-  EXPECT_EQ(true, (my_vector.z() - 3.0 < kEpsilon));
+  EXPECT_EQ(true, AreEqual(another_vector.x(), 1.0));
+  EXPECT_EQ(true, AreEqual(another_vector.y(), 2.0));
+  EXPECT_EQ(true, AreEqual(another_vector.z(), 3.0));
 }
 
 // Incluya tests adicionales para el resto de métodos de la clase
-
 
 
 /**
